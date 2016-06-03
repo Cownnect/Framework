7d4815a8dbcbe9980c90a37654c5e4a758dfd2b9
Cownnect <contact@cownnect.eu>
2016-06-03 13:31:37 +0200

    Update Pagination.php

--- Plugins/Pagination.php
+++ Plugins/Pagination.php
@@ -9,84 +9,82 @@
 
 $q = $db->query('SELECT id FROM ' . $table);
 
-//Récupération du nombre total de lignes
+//Total lines
 $nbr_total = $q->rowCount();
 
 //Récupération du nombre de lignes par pages
 $nbr_pages = $number;
 
-//Nombres de pages avant et après la page courante
+//Number of lines per page
 $nbr_aa_pages = 4;
 
-//Récupération de la dernière page
+//Last page
 $last_page = ceil($nbr_total / $nbr_pages);
 
-//Récupération de la page courante
+//Current page
 if (isset($_GET['page']) && is_numeric($_GET['page'])) {
     $num_page = $_GET['page'];
 } else {
     $num_page = 1;
 }
 
-//Évitement de page supérieur à la dernière et inférieur à la première
 if ($num_page < 1) {
     $num_page = 1;
 } elseif ($num_page > $last_page) {
     $num_page = $last_page;
 }
 
-//Définition de la limit
+//Limit definition
 $limit = "LIMIT " . ($num_page - 1) * $nbr_pages . "," . $nbr_pages;
 
 $pagination = "";
 
-//Création de la pagination si il y a plus que 1 page
 if ($last_page != 1) {
 
-    //Si le numero de page est superieur à 1
+    //IF page number upper than 1
     if ($num_page > 1) {
 
-        //Alors ajout du bouton de retour à la première page
+        //Adding Fast Backward Button
         $pagination .= '<li><a href="?page=1"><i class="glyphicon glyphicon-fast-backward"></i></a></li>';
 
-        //Calcule de la page précédente
+        //Previous page
         $previous = $num_page - 1;
 
-        //Ajout du bouton de retour à la page précédente
+        //Adding Previous Button
         $pagination .= '<li><a href="?page=' . $previous . '"><i class="glyphicon glyphicon-chevron-left"></i></a></li>';
 
-        //Ajout des pages précédent la page courante
+        //Previous pages from current page
         for ($i = $num_page - $nbr_aa_pages; $i < $num_page; $i++) {
             if ($i > 0) {
+                //Adding Previous Page Button
                 $pagination .= '<li><a href="?page=' . $i . '">' . $i . '</a></li>';
             }
         }
     }
 
-    //Ajout de la page courante
+    //Adding Current Page Button
     $pagination .= '<li class="disabled" style="font-weight: bold;"><a>' . $num_page . '</a></li>';
 
-    //Ajout des pages suivant la page courante
+    //Next pages from current page
     for ($i = $num_page + 1; $i <= $last_page; $i++) {
+
+        //Adding Next Page Button
         $pagination .= '<li><a href="?page=' . $i . '">' . $i . '</a></li>';
 
-        //Arret de l'ajout des pages
         if ($i >= $num_page + $nbr_aa_pages) {
             break;
         }
     }
 
-    //Si la page courante n'est pas la dernière
     if ($num_page != $last_page) {
-        //Calcule de la page suivante
+        //Next page
         $next = $num_page + 1;
 
-        //Ajout du bouton permettant d'aller à la page suivante
+        //Adding Next Button
         $pagination .= '<li class="next"><a href="?page=' . $next . '"><i class="glyphicon glyphicon-chevron-right"></i></a></li>';
 
-        //Ajout du bouton permettant d'aller à la dernière page
+        //Adding Fast Forward Button
         $pagination .= '<li><a href="?page=' . $last_page . '"><i class="glyphicon glyphicon-fast-forward"></i></a></li>';
     }
-
 
 }
