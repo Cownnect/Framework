edd983a4a5205aa5e963141271cb7f9a970bb59e
Cownnect <contact@cownnect.eu>
2016-06-03 14:04:22 +0200

    Update Pagination.php

--- Plugins/Pagination.php
+++ Plugins/Pagination.php
@@ -1,40 +1,20 @@
 <?php
 
-/*
- * Comments Plugin by Cownnect Developpers
+/* * * * * * * * * *
  *
- * Copyright (c) 2015 Cownnect
-
-    MIT license
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
- */
+ * Pagination Plugin by Cownnect Developers
+ *
+ * GitHub : https://github.com/Cownnect/
+ * * * * * * * * * */
 
 $q = $db->query('SELECT id FROM ' . $table);
 
 //Total lines
 $nbr_total = $q->rowCount();
 
-//Récupération du nombre de lignes par pages
+//Number of lines per page
 $nbr_pages = $number;
 
-//Number of lines per page
 $nbr_aa_pages = 4;
 
 //Last page
@@ -105,5 +85,6 @@ if ($last_page != 1) {
         //Adding Fast Forward Button
         $pagination .= '<li><a href="?page=' . $last_page . '"><i class="glyphicon glyphicon-fast-forward"></i></a></li>';
     }
+
 
 }
