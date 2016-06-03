b7bcc8801ba25bc4eafc254fbd5fd95987e74e84
Cownnect <contact@cownnect.eu>
2016-06-03 14:06:45 +0200

    Update Pagination - V1.php

--- Plugins/Pagination/Pagination - V1.php
+++ Plugins/Pagination/Pagination - V1.php
@@ -1,11 +1,32 @@
 <?php
 
-/* * * * * * * * * *
+/*
+ * Comments Plugin by Cownnect Developpers
  *
- * Pagination Plugin by Cownnect Developers
- *
- * GitHub : https://github.com/Cownnect/
- * * * * * * * * * */
+ * Copyright (c) 2015 Cownnect
+
+    MIT license
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+ */
+
+/* GITHUB : https://github.com/Cownnect/ */
 
 $q = $db->query('SELECT id FROM ' . $table);
 
